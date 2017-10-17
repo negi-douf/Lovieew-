6270347f494b699d76812f72799611e6d5431ff5
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-17 18:11:56 +0900

    settings for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+  end
 end
