c3c3535f7c974b7c6654d546162e2cdce967154b
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-17 12:27:03 +0900

    fix models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+  validates :content, presence: true
+  validates :object, presence: true
+  
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015130843) do
+ActiveRecord::Schema.define(version: 20171016235611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "object"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
