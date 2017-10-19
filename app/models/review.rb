81501c1d6ef1d257a2c18e7eeb6ae0966f168a26
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-19 12:40:20 +0900

    fix review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
-  validates :content, presence: true
-  validates :object, presence: true
-  
+  validates :title, length: { maximum: 50 }
+  validates :object, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 400 }
+
   belongs_to :user
 end
