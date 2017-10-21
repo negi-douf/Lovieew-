5c21cb5813a07cda72b2ffb947115c4db6dbba67
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-21 17:08:08 +0900

    fix review/index, add fixing carrierwaves NameError

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,8 @@ module Lovieew
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     config.active_record.raise_in_transactional_callbacks = true
+
+    # CarrierWave NameError 回避
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
