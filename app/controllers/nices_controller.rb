7824c5fd5ec75db3e6a5c96f6fc2309d7712c874
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-22 20:17:15 +0900

    fix nices_controller

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -1,9 +1,18 @@
 class NicesController < ApplicationController
   before_action :authenticate_user!
+  respond_to :js
 
   def create
+    @nice = current_user.nice.build(nice_params)
+    @nice.save
   end
 
   def destroy
   end
+
+  private
+
+    def nice_params
+      params.require(:nice).premit(:review_id)
+    end
 end
