f6bb53e4f6a4afc7e5314beb969128624bba35b2
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-23 20:36:21 +0900

    add nices#create, destroy 2/3

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -3,22 +3,24 @@ class NicesController < ApplicationController
   respond_to :js
 
   def create
-    @nice = current_user.nices.build(nice_params)
+    @nice = current_user.nices.build(review_id: params[:nice][:review_id])
+    # @nice.user_id = current_user.id
+    # @nice = Nice.find(params[:nice][:review_id])
     @review = @nice.review
     @nice.save
     respond_with @review
   end
 
   def destroy
-    @nice = Nice.find(nice_params(:id))
+    @nice = Nice.find(params[:id])
     @review = @nice.review
     @nice.destroy
     respond_with @review
   end
 
-  private
-
-    def nice_params
-      params.require(:nice).permit(:review_id)
-    end
+#  private
+#
+#    def nice_params
+#      params.require(:nice).permit(:review_id)
+#    end
 end
