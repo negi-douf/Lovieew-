c8a394a2ffb95bae4dd3fad4e1972673220c2893
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-23 21:14:32 +0900

    fix nice buttons 1/2

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -4,8 +4,6 @@ class NicesController < ApplicationController
 
   def create
     @nice = current_user.nices.build(review_id: params[:nice][:review_id])
-    # @nice.user_id = current_user.id
-    # @nice = Nice.find(params[:nice][:review_id])
     @review = @nice.review
     @nice.save
     respond_with @review
@@ -17,10 +15,4 @@ class NicesController < ApplicationController
     @nice.destroy
     respond_with @review
   end
-
-#  private
-#
-#    def nice_params
-#      params.require(:nice).permit(:review_id)
-#    end
 end
