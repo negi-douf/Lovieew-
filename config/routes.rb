c3007caef51f61ce6ff84c80574ea39e62f34fd9
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-10-29 18:22:32 +0900

    change root to reviews#ranking

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     collection do
       get "ranking"
     end
-    
+
     resources :comments
   end
 
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
 
-  root "reviews#index"
+  root "reviews#ranking"
 end
